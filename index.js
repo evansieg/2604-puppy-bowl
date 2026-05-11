f037982905f3d39543a737f63f5966475fd3471f
Evan Siegel <evan@casasiegel.com>
2026-05-11 13:42:35 -0600

    Completed, added pseudo code to ensure I understood what each line did

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // === Constants ===
-const BASE = "https://fsa-puppy-bowl.herokuapp.com/api";
-const COHORT = "/"; // Make sure to change this!
+const BASE = "https://fsa-puppy-bowl.herokuapp.com/api/2604/players";
+const COHORT = "/2604-Evan"; // Make sure to change this!
 const API = BASE + COHORT;
